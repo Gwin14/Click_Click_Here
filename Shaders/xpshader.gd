0bc293419688eb0531ceb22e6309fe19c7a84bfa
Fábio Santos <fabiospinheira@gmail.com>
2025-10-06 11:44:32 -0300

    new game name and email icons

--- Shaders/xpshader.gd
+++ Shaders/xpshader.gd
@@ -1,5 +1,12 @@
 extends Control  # ou TextureRect
 
+# Permite definir no editor se o nó começa visível
+@export var start_visible: bool = true
+
+func _ready():
+	# Define a visibilidade inicial
+	visible = start_visible
+	
 func _process(delta):
 	if material:  # pega o material do próprio nó
 		material.set_shader_parameter("time", Time.get_ticks_msec() / 1000.0)
